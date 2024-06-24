cff025aef501d9859298a38f0135c03f78c2aedd
Piootrekk <piotr.wilkowiecki100@gmail.com>
2024-06-24 22:28:01 +0200

    init

---
+++ links-preparer/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+const App = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold underline text-red-800">
+        Hello world!
+      </h1>
+      <Button variant="outline" size="lg">
+        Click me!
+      </Button>
+    </>
+  );
+};
+
+export default App;
